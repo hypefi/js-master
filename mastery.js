f4567a4b0c05aca98fecd58bbbb1ccf22f8c6f2d
Anas <anasboukharta@gmail.com>
2019-12-10 13:02:37 +0100

    reinitialized files

---
+++ mastery.js
@@ -0,0 +1,21 @@
+// js mastery
+
+require('/Users/admin/desktop/JS_learning/scripts.js')
+
+function map(array, transform) {
+	let mapped = [];
+	for (let element of array){
+		mapped.push(transform(element));
+	}
+return mapped;
+}
+
+let rtlScripts = SCRIPTS.filter(s => s.direction == "rtl");
+
+console.log(map(rtlScripts, s => s.name));
+
+
+let ltrScripts = SCRIPTS.filter(s => s.direction == "ltr");
+
+console.log(map(ltrScripts, s => s.name));
+
